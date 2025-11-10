63043d387ffb87fc27a7e05e5ae81174e6828a93
akasan-code <aaanohito88@gmail.com>
2025-11-10 14:02:13 +0900

    Create script.js

---
+++ dungeonGame/script.js
@@ -0,0 +1,11 @@
+// ログ出力用の関数
+function addLog(message) {
+  const log = document.getElementById("log");
+  log.innerHTML += "> " + message + "<br>";
+  log.scrollTop = log.scrollHeight; // 自動スクロール
+}
+
+// 進むボタンが押されたときの処理
+function moveForward() {
+  addLog("あなたは前に進んだ。");
+}
